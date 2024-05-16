b5ab5983e3d28b18aad99545691572d599176f5d
WerrerXD <andreyperko9@gmail.com>
2024-05-16 12:48:30 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/OrderServiceImplementation.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    class OrderServiceImplementation : IOrderService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public OrderServiceImplementation(ApplicationDbContext dbContext)
+        {
+
+            _dbContext = dbContext;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+
+            var order = await _dbContext.Orders
+                .Where(o => o.Quantity > 1)
+                .OrderByDescending(o => o.CreatedAt)
+                .FirstOrDefaultAsync();
+
+            return order;
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            var orders = await _dbContext.Orders
+             .Include(o => o.User)
+             .Where(o => o.User.Status == UserStatus.Active)
+             .OrderBy(o => o.CreatedAt)
+             .Select(o => new Order
+             {
+                 Id = o.Id,
+                 UserId = o.UserId,
+                 ProductName = o.ProductName,
+                 Price = o.Price,
+                 Quantity = o.Quantity,
+                 CreatedAt = o.CreatedAt,
+                 Status = o.Status,
+             })
+             .ToListAsync();
+
+            return orders;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserServiceImplementation.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserServiceImplementation: IUserService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserServiceImplementation(ApplicationDbContext dbContext)
+        {
+
+            _dbContext = dbContext;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var userWithMaxOrderTotal = await _dbContext.Orders
+            .Where(o => o.CreatedAt.Year == 2003)
+            .GroupBy(o => o.UserId)
+            .Select(g => new
+            {
+                UserId = g.Key,
+                TotalOrderAmount = g.Sum(o => o.Quantity * o.Price)
+            })
+            .OrderByDescending(x => x.TotalOrderAmount)
+            .Select(x => x.UserId)
+            .FirstOrDefaultAsync();
+
+            if (userWithMaxOrderTotal == null)
+                return null;
+
+            var user = await _dbContext.Users.FindAsync(userWithMaxOrderTotal);
+            return user;
+
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var usersWithPaidOrdersIn2010 = await _dbContext.Users
+            .Where(u => u.Orders.Any(o => o.CreatedAt.Year == 2010 && o.Status == OrderStatus.Paid))
+            .ToListAsync();
+
+            return usersWithPaidOrdersIn2010;
+        }
+    }
+}
