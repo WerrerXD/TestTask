0d6a2df79b2eda2abdb7f552fb513cb0c4a435a0
WerrerXD <andreyperko9@gmail.com>
2024-05-16 12:54:32 +0300

    -

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddTransient<IOrderService,OrderServiceImplementation>();
+builder.Services.AddTransient<IOrderService, OrderServiceImplementation>();
 builder.Services.AddTransient<IUserService, UserServiceImplementation>();
         
 builder.Services.AddControllers();
